bf939db49a4105bf54339978e964b4315de0e7ad
Sereda-Fazen <fazen7@mail.ru>
2016-04-11 16:59:57 +0300

    mowDirect-4

--- tests/acceptance/02_LoginCest.php
+++ tests/acceptance/02_LoginCest.php
@@ -20,25 +20,28 @@ class LoginCest
     }
 
     function loginEmptyFields(AcceptanceTester $I, \Page\Login $loginPage) {
-
+        $loginPage->login();
         $loginPage->loginInvalid('', '');
         $I->see( 'This is a required field.','#advice-required-entry-pass');
         $I->comment('Expected result: This is a required field pass and email.');
     }
 
     function loginEmptyPass(AcceptanceTester $I, \Page\Login $loginPage) {
+        $loginPage->login();
         $loginPage->loginInvalid('test@test.org', '');
         $I->see( 'This is a required field.','#advice-required-entry-pass');
         $I->comment('Expected result: This is a required field pass.');
     }
 
     function loginEmptyEmail(AcceptanceTester $I, \Page\Login $loginPage) {
+        $loginPage->login();
         $loginPage->loginInvalid('', '123456');
         $I->see( 'This is a required field.','#advice-required-entry-email');
         $I->comment('Expected result: This is a required field email.');
     }
 
     function loginInvalidEmail(AcceptanceTester $I, \Page\Login $loginPage) {
+        $loginPage->login();
         $loginPage->loginInvalid('testemail.com', '123456');
         $I->see('Please enter a valid email address. For example johndoe@domain.com.', '#advice-validate-email-email');
         $I->comment('Expected result: Please enter a valid email address.');
