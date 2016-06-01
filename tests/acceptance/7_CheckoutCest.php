57c7feabfeae2888f60e5f2a3a8e7c664f89eb52
Sereda-Fazen <fazen7@mail.ru>
2016-06-01 17:18:31 +0300

    mowDirect -7

---
+++ tests/acceptance/7_CheckoutCest.php
@@ -0,0 +1,79 @@
+<?php
+use \Step\Acceptance;
+
+/**
+ * @group checkout
+ */
+class CheckoutCest
+{
+
+    /**
+     * @param Acceptance\LoginSteps $I
+     * T918_Tractor Sale
+     */
+
+    function loginSuccess(Step\Acceptance\LoginSteps $I) {
+        $I->loginSuccess('mowdirect@gmail.com','123456');
+    }
+    
+    function addToBasketTractor(\Step\Acceptance\CheckoutSteps $I){
+        $I->addToBasketTractor();
+    }
+
+    function checkOrderTractor(\Page\Checkout $checkoutPage){
+        $checkoutPage->checkOrder();
+    }
+
+    /**
+     * @param Acceptance\CheckoutSteps $I
+     * T919_Mower Sale
+     */
+
+    function addToBasketMower(\Step\Acceptance\CheckoutSteps $I){
+        $I->addToBasketMower();
+    }
+
+    function checkOrderMower(\Page\Checkout $checkoutPage){
+        $checkoutPage->checkOrder();
+    }
+
+
+    /**
+     * @param Acceptance\CheckoutSteps $I
+     * T920_Purchase Tractor with other product
+     */
+
+
+    function addToBasketTractorForOther(\Step\Acceptance\CheckoutSteps $I){
+        $I->addToBasketTractor();
+    }
+    function purchaseOtherItem(\Step\Acceptance\CheckoutSteps $I){
+        $I->purchaseOtherItem();
+    }
+
+    function checkOrderTractorOther(\Page\Checkout $checkoutPage){
+        $checkoutPage->checkOrder();
+    }
+
+    /**
+     * @param Acceptance\CheckoutSteps $I
+     * T922_Purchase Multiple Different Products Same supplie
+     */
+
+
+    function selectBrandTwoBrands(\Step\Acceptance\CheckoutSteps $I){
+        $I->selectBrandTwoBrands();
+    }
+
+    function checkOrderMultipleTractor(\Page\Checkout $checkoutPage)
+    {
+        $checkoutPage->checkOrder();
+    }
+
+
+
+    
+
+
+}
+
