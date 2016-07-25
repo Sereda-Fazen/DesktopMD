3d878d3f28856ef295a84260c26a5675633ab4c4
Yuriy <yf@itsvit.org>
2016-07-25 15:51:43 +0300

    Test

--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -7,6 +7,39 @@ use \Step\Acceptance;
 class TestCest
 {
 
+
+//
+    function T1017TractorSale(\Step\Acceptance\CheckoutSteps $I,\Page\Checkout $checkoutPage){
+        $I->addToBasketTractor();
+        $checkoutPage->checkPayment('mowdirect@gmail.com','123456');
+    }
+
+    function T1018MowerSale(\Step\Acceptance\CheckoutSteps $I,\Page\Checkout $checkoutPage){
+        $I->addToBasketMower();
+        $checkoutPage->checkPayment('mowdirect@gmail.com','123456');
+    }
+
+
+    function T1026MakeSugePurchase(Step\Acceptance\CheckoutSteps $I, \Page\Checkout $checkoutPage) {
+        $I->addToBasketTractor();
+        $checkoutPage->checkSugePurchase('mowdirect@gmail.com','123456', 'American Express', '378282246310005','1234');
+    }
+
+    function T1027PayPalPurchase(Step\Acceptance\CheckoutSteps $I, \Page\Checkout $checkoutPage) {
+        $I->addToBasketTractor();
+        $checkoutPage->payPalCheckout();
+    }
+
+    function T1266PayPalCredit(Step\Acceptance\CheckoutSteps $I, \Page\Checkout $checkoutPage) {
+        $I->addToBasketTractor();
+        $checkoutPage->payPalCredit();
+    }
+
+    function T1267PayPalCreditCheckout(Step\Acceptance\CheckoutSteps $I, \Page\Checkout $checkoutPage) {
+        $I->addToBasketTractor();
+        $checkoutPage->checkPayPalCredit('mowdirect@gmail.com','123456');
+    }
+
     function T995T999CheckMyWishlist(Step\Acceptance\EmailSteps $I, \Page\MyWishList $myWishList)
     {
         $I->loginSuccess('test_mowdirect@yahoo.co.uk', '123456');
@@ -19,6 +52,13 @@ class TestCest
         $myWishList->removeItem();
     }
 
+
+    function loginSuccess(AcceptanceTester $I, \Page\Login $loginPage) {
+        $loginPage->login();
+        $loginPage->loginInvalid('test_mowdirect@yahoo.co.uk', '123456');
+        $I->see('From your My Account Dashboard you have the ability to view','div.welcome-msg');
+        $loginPage->logout();
+    }
   
 }
 
