8196e99a3b757828b17ed24d8e59c486b135c45a
Sereda-Fazen <fazen7@mail.ru>
2016-11-01 15:53:47 +0200

    update CC 2.2.6 on the Wayne's instance - 1

--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -9,7 +9,10 @@ class TestCest
 {
 
 
-   
+    /**
+     * @tr-suite R32
+     * @tr-case T1359
+     */
 
     function T1359UseCategoryNavigationToNavigateToTheVariousSaleDepartments(Page\CategoryNavigation $categoryNavigation, \Step\Acceptance\ProductsSteps $I)
     {
